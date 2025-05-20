90b1b7132effde40f49cc35dc1036652b76ba686
brillianintdoh <wheedo07@ourgram.co.kr>
2025-05-20 20:00:01 +0900

    버그 수정

--- cpp/src/overworld/trueLaboratory/room/4.cpp
+++ cpp/src/overworld/trueLaboratory/room/4.cpp
@@ -11,23 +11,26 @@ void TrueLaboratory::potato_trigger() {
 
 void TrueLaboratory::potato_select(int index) {
     if(index != 0) return;
-    int gold = global->get_player_gold();
-    gold-=25;
-    if(gold >= 0) {
-        if(global->get_items().size() == 8) {
-            summontextbox()->generic(sys->dia()->from({
-                String::utf8("* 당신의 인벤토리가 가득찼다")
-            }));
-        }else {
-            summontextbox()->generic(sys->dia()->from({
-                String::utf8("* ( 자판기에서 감자칩이 나왔다. )")
-            }));
-            global->set_player_gold(gold);
-            Array items = global->get_items();
-            items.append(8);
-            global->set_items(items);
-        }
-    }else summontextbox()->generic(sys->dia()->from({
-        String::utf8("* ( 돈이 부족하다.. )")
-    }));
+    sys->executeTrue([this]() { return !global->get_player_text_box(); },
+    [this]() {
+        int gold = global->get_player_gold();
+        gold-=25;
+        if(gold >= 0) {
+            if(global->get_items().size() == 8) {
+                summontextbox()->generic(sys->dia()->from({
+                    String::utf8("* 당신의 인벤토리가 가득찼다")
+                }));
+            }else {
+                summontextbox()->generic(sys->dia()->from({
+                    String::utf8("* ( 자판기에서 감자칩이 나왔다. )")
+                }));
+                global->set_player_gold(gold);
+                Array items = global->get_items();
+                items.append(8);
+                global->set_items(items);
+            }
+        }else summontextbox()->generic(sys->dia()->from({
+            String::utf8("* ( 돈이 부족하다.. )")
+        }));
+    });
 }
